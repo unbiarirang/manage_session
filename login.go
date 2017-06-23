b003f7419016458e72ca3cd8baa79da48415aad8
최은비 <ebchoi@bulkytree.com>
2017-06-23 18:50:48 +0900

    수정

--- login.go
+++ login.go
@@ -5,13 +5,15 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/satori/go.uuid"
 
 	"bulkytree.com/sevenhearts/auth"
 )
 
-const maxLifetime int64 = 3600
+const maxLifetime int64 = 5
+const defaultLifetime int64 = 5
 
 type UUID uuid.UUID
 
@@ -19,31 +21,41 @@ func (u UUID) String() string {
 	return uuid.UUID(u).String()
 }
 
-var sessions = make(map[UUID]session)
+var sessions map[UUID]session
 var SessionManager *sessionManager
 
 func init() {
-	SessionManager = NewSessionManager(maxLifetime)
+	sessions = NewSessions()
+	SessionManager = NewSessionManager(defaultLifetime, sessions)
+	SessionManager.SessionGC()
 }
 
 type sessionManager struct {
-	lock        sync.Mutex
-	maxLifetime int64
+	lock       sync.RWMutex
+	sessions   map[UUID]session
+	gcLifetime int64
 }
 
 type session interface {
-	Set(key string, value interface{}) //set session value
-	Get(key string) interface{}        //get session value
-	Delete(key string)                 //delete session value
-	SessionKey() UUID                  //back current sessionID
+	Set(key string, value interface{})  //set session value
+	Get(key string) (interface{}, bool) //get session value
+	Delete(key string)                  //delete session value
+
+	SessionKey() UUID //back current sessionID
+	//setSessionKey(skey UUID)
+	Expiry() int64 //back current expiry
+	//setExpiry(v int64)
 }
 
 type sessionObj struct {
-	session map[string]interface{}
+	session    map[string]interface{}
+	sessionKey UUID
+	expiry     int64
 }
 
-func (s sessionObj) Get(key string) interface{} {
-	return s.session[key]
+func (s sessionObj) Get(key string) (interface{}, bool) {
+	v, ok := s.session[key]
+	return v, ok
 }
 
 func (s sessionObj) Set(key string, value interface{}) {
@@ -51,58 +63,101 @@ func (s sessionObj) Set(key string, value interface{}) {
 }
 
 func (s sessionObj) Delete(key string) {
-	_, ok := s.session[key]
-	if ok {
+	if _, ok := s.session[key]; ok {
 		delete(s.session, key)
 	}
 }
 
 func (s sessionObj) SessionKey() UUID {
-	return s.session["sessionKey"].(UUID)
+	return s.sessionKey
 }
 
-func (manager *sessionManager) NewSessionKey() UUID {
+func (s sessionObj) Expiry() int64 {
+	return s.expiry
+}
+
+func (s *sessionObj) setSessionKey(sKey UUID) {
+	s.sessionKey = sKey
+}
+
+func (s *sessionObj) setExpiry(v int64) {
+	s.expiry = v
+}
+
+func (m *sessionManager) NewSessionKey() UUID {
 	return UUID(uuid.NewV4())
 }
 
-func (manager *sessionManager) sessionInit(sKey UUID) session {
+func (m *sessionManager) sessionInit(sKey UUID) session {
 	s := sessionObj{}
 	s.session = make(map[string]interface{})
-	s.Set("sessionKey", sKey)
-	sessions[sKey] = s
+	s.setSessionKey(sKey)
+	s.setExpiry(time.Now().Unix() + m.gcLifetime)
+	m.sessions[sKey] = s
 	return s
 }
 
-func (manager *sessionManager) SessionRead(sKey UUID) session {
-	s, ok := sessions[sKey]
-	if ok {
-		return manager.sessionInit(sKey)
+func (m *sessionManager) SessionRead(sKey UUID) session {
+	m.lock.RLock()
+	s, ok := m.sessions[sKey]
+	m.lock.RUnlock()
+
+	if !ok {
+		m.lock.Lock()
+		defer m.lock.Unlock()
+		return m.sessionInit(sKey)
 	}
 	return s
 }
 
-func (manager *sessionManager) SessionDestroy(sKey UUID) {
-	_, ok := sessions[sKey]
-	if ok {
-		delete(sessions, sKey)
+func (m *sessionManager) SessionDestroy(sKey UUID) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if _, ok := m.sessions[sKey]; ok {
+		delete(m.sessions, sKey)
 	}
 }
 
-func (manager *sessionManager) SessionGC(maxLifetime int64) {
+func (m *sessionManager) SessionGC() {
+	if m.gcLifetime == 0 {
+		return
+	}
 
+	for key := range m.sessions {
+		if m.sessions[key].Expiry() <= time.Now().Unix() {
+			m.SessionDestroy(key)
+		}
+	}
+
+	fmt.Printf("\nIn GC!, %v\n timenow: %v\n", m.sessions, time.Now().Unix())
+
+	time.AfterFunc(time.Duration(m.gcLifetime)*time.Second, func() { m.SessionGC() })
 }
 
-func (manager *sessionManager) SessionStart() (session session) {
-	manager.lock.Lock()
-	defer manager.lock.Unlock()
-	sKey := manager.NewSessionKey()
-	session = manager.sessionInit(sKey)
+func (m *sessionManager) SessionStart() (session session) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	sKey := m.NewSessionKey()
+	session = m.sessionInit(sKey)
 	fmt.Printf("session: %v, sessionKey: %v, sKey: %v", session, session.SessionKey(), sKey)
 	return session
 }
 
-func NewSessionManager(maxLifetime int64) *sessionManager {
-	return &sessionManager{maxLifetime: maxLifetime}
+func NewSessionManager(gcLifetime int64, sessions map[UUID]session) *sessionManager {
+	if maxLifetime == 0 {
+		return &sessionManager{gcLifetime: maxLifetime, sessions: sessions}
+	}
+
+	if gcLifetime > maxLifetime {
+		gcLifetime = maxLifetime
+	}
+	return &sessionManager{gcLifetime: gcLifetime, sessions: sessions}
+}
+
+func NewSessions() map[UUID]session {
+	return make(map[UUID]session)
 }
 
 func login(platformType, platformID, IDToken string) error {
--- login_test.go
+++ login_test.go
@@ -1,12 +1,19 @@
 package login
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 // TODO:
-// 1. GC 만드세요. maxLife/defaultLife인지 분명히 하세요
-// 2. NewSessionManager 가 있으면 대응되는 sessions (session store) 생성자도 있어야 합니다. 아니면 아예 둘다 지원하지 않는것도 방법
-// 3. lock 처리가 빠져 있습니다. 보강 하세요.
-// 4. map get set ok처리 (nil도 처리)
+// 1. GC 만드세요. maxLife/defaultLife인지 분명히 하세요 @
+// 2. NewSessionManager 가 있으면 대응되는 sessions (session store) 생성자도 있어야 합니다. 아니면 아예 둘다 지원하지 않는것도 방법 @
+// 3. lock 처리가 빠져 있습니다. 보강 하세요. @
+// 4. map get set ok처리 (nil 처리) @
+
+// Q
+// 1. sessionKey랑 expiry 맵에서 겟할 때와 타입 캐스팅할 때 에러처리 필요한가? @ 맵에서 빼고 세션 오브젝트의 필드로 바꿈
+// 2. 하나의 session 안에서도 lock이 필요할까? @ (A:No)
 
 func TestSession(t *testing.T) {
 	s := SessionManager.SessionStart()
@@ -16,7 +23,8 @@ func TestSession(t *testing.T) {
 
 	intValue := 1
 	s.Set("a", intValue)
-	intResult, ok := s.Get("a").(int)
+	intResult, ok := s.Get("a")
+	intResult = intResult.(int)
 	if !ok || intResult != intValue {
 		t.Error("get set int fail")
 	}
@@ -24,11 +32,14 @@ func TestSession(t *testing.T) {
 	s1 := SessionManager.SessionStart()
 	s1.Set("b", 2)
 	s1.Delete("b")
-	if s1.Get("b") != nil {
+	_, ok = s1.Get("b")
+	if ok == true {
 		t.Error("b 삭제는 성공해야 한다")
 	}
 
 	if len(sessions) != 2 {
 		t.Errorf("sessions에는 두 개의 session이 존재햐야 한다. len: %v != 2", len(sessions))
 	}
+
+	time.Sleep(time.Second * 20)
 }
